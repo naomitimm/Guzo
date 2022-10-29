af7c55efeb36a8213f52fbb86311d4af413347c8
Naomi Timothy <naomitim17.com>
2022-10-29 16:39:34 +0300

    login-bloc2

--- guzo_app/lib/application/auth/login/bloc/login_bloc.dart
+++ guzo_app/lib/application/auth/login/bloc/login_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:guzo_app/application/auth/form_submission_state.dart';
 import 'package:meta/meta.dart';
 
@@ -12,7 +13,15 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     on<LoginFormSubmitted>(_handleLoginFormSubmission);
   }
 
-  _handleLoginEmailChange(LoginEmailChanged event, Emitter emit) {}
-  _handleLoginPasswordChange(LoginPasswordChanged event, Emitter emit) {}
-  _handleLoginFormSubmission(LoginFormSubmitted event, Emitter emit) {}
+  _handleLoginEmailChange(LoginEmailChanged event, Emitter emit) async {
+    state.copyWith(email: event.email);
+  }
+
+  _handleLoginPasswordChange(LoginPasswordChanged event, Emitter emit) async {
+    state.copyWith(password: event.password);
+  }
+
+  _handleLoginFormSubmission(LoginFormSubmitted event, Emitter emit) async {
+    state.copyWith(formState: const FormSubmissionLoading());
+  }
 }
