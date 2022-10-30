b204334f47f24a5e67a54eb20e85bb87457886f4
Naomi Timothy <naomitim17.com>
2022-10-30 23:37:50 +0300

    signup-bloc2

--- guzo_app/lib/application/auth/signup/signup_bloc.dart
+++ guzo_app/lib/application/auth/signup/signup_bloc.dart
@@ -1,13 +1,23 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
+import 'package:guzo_app/presentation/exports.dart';
 
 part 'signup_event.dart';
 part 'signup_state.dart';
 
 class SignupBloc extends Bloc<SignupEvent, SignupState> {
-  SignupBloc() : super(SignupInitial()) {
-    on<SignupEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final AuthRepository authRepository;
+  SignupBloc({required this.authRepository}) : super(const SignupInitial()) {
+    on<SignupRequested>(_handleSignupRequest);
+  }
+
+  void _handleSignupRequest(SignupEvent event, Emitter emit) async {
+    try {
+      emit(const SigningUp());
+      await authRepository.signup();
+      emit(const SignupSuccessful());
+    } on Error catch (error) {
+      emit(SignupFailed(error: error));
+      return;
+    }
   }
 }
--- guzo_app/lib/application/auth/signup/signup_event.dart
+++ guzo_app/lib/application/auth/signup/signup_event.dart
@@ -1,4 +1,16 @@
 part of 'signup_bloc.dart';
 
 @immutable
-abstract class SignupEvent {}
+abstract class SignupEvent extends Equatable {}
+
+class SignupRequested extends SignupEvent {
+  SignupRequested(
+      {required this.userName, required this.email, required this.password});
+
+  final String userName;
+  final String email;
+  final String password;
+
+  @override
+  List<Object?> get props => [userName, email, password];
+}
